61c5ae289d8d1c2ccdfc083240fa3148ba71d8a8
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-09-10 17:55:21 +0430

    ..

---
+++ app/src/main/java/com/example/adp_47_ecc/EmailLoginRegister/EmailLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.adp_47_ecc.EmailLoginRegister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.adp_47_ecc.R;
+
+public class EmailLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_login);
+    }
+}
